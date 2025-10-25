a86deca66eb00d3fdbf05bf4c5b195f91f919df0
Your Name <you@example.com>
2025-10-25 11:26:23 -0500

    fix build file

--- build.sh
+++ build.sh
@@ -3,9 +3,5 @@
 # Exit on error
 set -e
 
-# Copy mock google-services file
-echo "Using mock google-services.json"
-cp mock-google-services.json app/google-services.json
-
 # Build
 ./gradlew clean build
